a0b0f2f71f179fd7945eb7ab30ad784fb9618033
Rohan Sharma <68840167+Sustainability4@users.noreply.github.com>
2021-06-06 19:30:59 +0530

    Create bubble_sort.cpp
    Bubble Sort (Iterative) LinkedList
    Send Feedback
    Given a singly linked list of integers, sort it using 'Bubble Sort.'
    Note :
    No need to print the list, it has already been taken care. Only return the new head to the list.
    Input format :
    The first and the only line of each test case or query contains the elements of the singly linked list separated by a single space.
    Remember/Consider :
    While specifying the list elements for input, -1 indicates the end of the singly linked list and hence, would never be a list element
    Output format :
    For each test case/query, print the elements of the sorted singly linked list.
    
    Output for every test case will be printed in a seperate line.
    Constraints :
    0 <= M <= 10^3
    Where M is the size of the singly linked list.
    
    Time Limit: 1sec
    Sample Input 1 :
    10 9 8 7 6 5 4 3 -1
    Sample Output 1 :
     3 4 5 6 7 8 9 10 
     Sample Output 2 :
    10 -5 9 90 5 67 1 89 -1
    Sample Output 2 :
    -5 1 5 9 10 67 89 90

---
+++ Linked_list/bubble_sort.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+class Node
+{
+public:
+	int data;
+	Node *next;
+	Node(int data)
+	{
+		this->data = data;
+		this->next = NULL;
+	}
+};
+
+using namespace std;
+
+Node *bubbleSort(Node *head)
+{
+	// Write your code here
+    if(head == NULL || head->next == NULL){
+        return head;
+    }
+    
+    int count = 0;
+    Node*cur = head;
+    Node*prev = head;
+    
+    while(cur != NULL && cur->next != NULL){
+        if(cur->data <= cur->next->data){
+            prev = cur;
+            cur = cur->next;
+        }else{
+            Node*a = cur->next;
+            cur->next = a->next;
+            a->next = cur;
+            prev->next = a;
+            prev = cur;
+            cur = cur->next;
+            count++;
+        }
+    }
+    
+    if(count == 0){
+        return head;
+    }else{
+        bubbleSort(head);
+    }
+    
+    return head;
+}
+
+Node *takeinput()
+{
+	int data;
+	cin >> data;
+	Node *head = NULL, *tail = NULL;
+	while (data != -1)
+	{
+		Node *newnode = new Node(data);
+		if (head == NULL)
+		{
+			head = newnode;
+			tail = newnode;
+		}
+		else
+		{
+			tail->next = newnode;
+			tail = newnode;
+		}
+		cin >> data;
+	}
+	return head;
+}
+void print(Node *head)
+{
+	Node *temp = head;
+	while (temp != NULL)
+	{
+		cout << temp->data << " ";
+		temp = temp->next;
+	}
+	cout << endl;
+}
+int main()
+{
+	Node *head = takeinput();
+	head = bubbleSort(head);
+	print(head);
+}
