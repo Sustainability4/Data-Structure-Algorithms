9644c294d46fdb7335c6e1c8a5eb7aa3e287a6c0
Rohan Sharma <68840167+Sustainability4@users.noreply.github.com>
2021-06-27 11:36:36 +0530

    Create binary_tree_zig_zag.cpp
    ZigZag tree
    Send Feedback
    Given a binary tree, print the zig zag order. In zigzag order, level 1 is printed from left to right, level 2 from right to left and so on. This means odd levels should get printed from left to right and even level right to left.
    Input format:
    The first line of input contains data of the nodes of the tree in level order form. The data of the nodes of the tree is separated by space. If any node does not have a left or right child, take -1 in its place. Since -1 is used as an indication whether the left or right nodes exist, therefore, it will not be a part of the data of any node.
    Output Format:
    The binary tree is printed level wise, as described in the task. Each level is printed in new line.
    Constraints
    Time Limit: 1 second
    Sample Input :
    5 6 10 2 3 -1 -1 -1 -1 -1 9 -1 -1
    Sample Output :
    5
    10 6
    2 3
    9

---
+++ trees and binary search trees/binary_tree_zig_zag.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+#include <queue>
+
+template <typename T>
+class BinaryTreeNode {
+   public:
+    T data;
+    BinaryTreeNode<T>* left;
+    BinaryTreeNode<T>* right;
+
+    BinaryTreeNode(T data) {
+        this->data = data;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+using namespace std;
+
+#include<stack>
+
+void zigZagOrder(BinaryTreeNode<int> *root) {
+    // Write your code here
+    
+    if(root == NULL){
+        return;
+    }
+    stack<BinaryTreeNode<int> *> s1_l_r;
+    stack<BinaryTreeNode<int> *> s2_r_l; 
+    
+    s1_l_r.push(root);
+    
+    while(s1_l_r.size() != 0 || s2_r_l.size() !=0){
+        if(s2_r_l.size() == 0){
+            while(s1_l_r.size() !=0){
+                cout<< s1_l_r.top()->data << " ";
+                if(s1_l_r.top()->left != NULL){
+                    s2_r_l.push(s1_l_r.top()->left);
+                }
+            	if(s1_l_r.top()->right != NULL){
+                    s2_r_l.push(s1_l_r.top()->right);
+                }
+            	s1_l_r.pop();
+            }
+            cout<<endl;
+        }else if (s1_l_r.size() == 0){
+            while(s2_r_l.size() != 0){
+                cout<<s2_r_l.top()->data<<" ";
+                if(s2_r_l.top()->right != NULL){
+                    s1_l_r.push(s2_r_l.top()->right);
+                }
+            	if(s2_r_l.top()->left != NULL){
+                    s1_l_r.push(s2_r_l.top()->left);
+                }
+            	s2_r_l.pop();
+            }
+            cout<<endl;
+
+        }
+    }
+}
+
+BinaryTreeNode<int>* takeInput() {
+    int rootData;
+    cin >> rootData;
+    if (rootData == -1) {
+        return NULL;
+    }
+    BinaryTreeNode<int>* root = new BinaryTreeNode<int>(rootData);
+    queue<BinaryTreeNode<int>*> q;
+    q.push(root);
+    while (!q.empty()) {
+        BinaryTreeNode<int>* currentNode = q.front();
+        q.pop();
+        int leftChild, rightChild;
+
+        cin >> leftChild;
+        if (leftChild != -1) {
+            BinaryTreeNode<int>* leftNode = new BinaryTreeNode<int>(leftChild);
+            currentNode->left = leftNode;
+            q.push(leftNode);
+        }
+
+        cin >> rightChild;
+        if (rightChild != -1) {
+            BinaryTreeNode<int>* rightNode =
+                new BinaryTreeNode<int>(rightChild);
+            currentNode->right = rightNode;
+            q.push(rightNode);
+        }
+    }
+    return root;
+}
+
+int main() {
+    BinaryTreeNode<int>* root = takeInput();
+    zigZagOrder(root);
+}
