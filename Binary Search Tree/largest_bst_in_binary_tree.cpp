1fd83acfcfcc6a848e442c72dbb2cfe419b9c477
Rohan Sharma <68840167+Sustainability4@users.noreply.github.com>
2021-07-06 18:14:47 +0530

    Create largest_bst_in_binary_tree.cpp
    Largest BST
    Send Feedback
    Given a Binary tree, find the largest BST subtree. That is, you need to find the BST with maximum height in the given binary tree. You have to return the height of largest BST.
    Input format :
    The first line of input contains data of the nodes of the tree in level order form. The data of the nodes of the tree is separated by space. If any node does not have left or right child, take -1 in its place. Since -1 is used as an indication whether the left or right nodes exist, therefore, it will not be a part of the data of any node.
    Output format:
    The first and only line of output contains the height of the largest BST.
    Constraints:
    Time Limit: 1 second
    Sample Input 1:
    5 10 6 2 3 -1 -1 -1 -1 -1 9 -1 -1
    Sample Output 1:
    2

---
+++ Binary Search Tree/largest_bst_in_binary_tree.cpp
@@ -0,0 +1,134 @@
+#include <iostream>
+#include <queue>
+
+template <typename T>
+class BinaryTreeNode {
+   public:
+    T data;
+    BinaryTreeNode<T>* left;
+    BinaryTreeNode<T>* right;
+
+    BinaryTreeNode(T data) {
+        this->data = data;
+        left = NULL;
+        right = NULL;
+    }
+    ~BinaryTreeNode() {
+        if (left) delete left;
+        if (right) delete right;
+    }
+};
+
+using namespace std;
+
+/**********************************************************
+
+	Following is the Binary Tree Node class structure
+
+	template <typename T>
+	class BinaryTreeNode {
+    	public: 
+    	T data;
+    	BinaryTreeNode<T> *left;
+    	BinaryTreeNode<T> *right;
+
+    	BinaryTreeNode(T data) {
+        	this->data = data;
+        	left = NULL;
+        	right = NULL;
+    	}
+	};
+	
+***********************************************************/
+#include<cmath>
+#include<limits>
+int height(BinaryTreeNode<int> * root){
+    if(root == NULL){
+        return 0;
+    }
+
+    return 1 + max(height(root->left),height(root->right));
+}
+
+bool isBSTHelp(BinaryTreeNode<int> *root, int min = -999999, int max = 99999999){
+    if(root == NULL){
+        return true;
+    }
+    
+    if(root->data <min || root->data>max){
+        return false;
+    }
+    
+    bool isleftok = isBSTHelp(root->left,min,root->data-1);
+    bool isrightok = isBSTHelp(root->right, root->data, max);
+    
+    return isleftok && isrightok;
+}
+
+bool isBST(BinaryTreeNode<int> *root) {
+	// Write your code here
+    
+    //IsBSTReturn  output = isBSTHelp(node);
+    //return output.IsBst;  
+    
+    bool a = isBSTHelp(root);
+    
+    return a;   
+    
+}
+
+int largestBSTSubtree(BinaryTreeNode<int> *root) {
+    // Write your code here
+    
+    if(root == NULL){
+        return 0; 
+    }
+    if(isBST(root)){
+        return height(root);
+    }
+    int height_left = largestBSTSubtree(root->left);
+    int height_right = largestBSTSubtree(root->right);
+    
+    return max(height_left,height_right);
+    
+    
+}
+
+BinaryTreeNode<int>* takeInput() {
+    int rootData;
+    
+    cin >> rootData;
+    if (rootData == -1) {
+        return NULL;
+    }
+    BinaryTreeNode<int>* root = new BinaryTreeNode<int>(rootData);
+    queue<BinaryTreeNode<int>*> q;
+    q.push(root);
+    while (!q.empty()) {
+        BinaryTreeNode<int>* currentNode = q.front();
+        q.pop();
+        int leftChild, rightChild;
+       
+        cin >> leftChild;
+        if (leftChild != -1) {
+            BinaryTreeNode<int>* leftNode = new BinaryTreeNode<int>(leftChild);
+            currentNode->left = leftNode;
+            q.push(leftNode);
+        }
+        
+        cin >> rightChild;
+        if (rightChild != -1) {
+            BinaryTreeNode<int>* rightNode =
+                new BinaryTreeNode<int>(rightChild);
+            currentNode->right = rightNode;
+            q.push(rightNode);
+        }
+    }
+    return root;
+}
+
+int main() {
+    BinaryTreeNode<int>* root = takeInput();
+    cout << largestBSTSubtree(root);
+    delete root;
+}
