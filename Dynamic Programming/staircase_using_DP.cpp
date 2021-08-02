f60796abbca03beb3043abc3f93491ae5b3815fb
Rohan Sharma <68840167+Sustainability4@users.noreply.github.com>
2021-08-02 11:04:43 +0530

    Create staircase_using_DP.cpp

---
+++ Dynamic Programming/staircase_using_DP.cpp
@@ -0,0 +1,25 @@
+//Dynamic Programming
+#include <iostream>
+using namespace std;
+long staircase(int n)
+{
+	//Write your code here
+    
+    long ans[n+1];
+    ans[0] = 1;
+    ans[1] = 1;
+    ans[2] = 2;
+    
+    for(int i = 3; i<=n ; i++){
+        ans[i] = ans[i-1]+ans[i-2]+ans[i-3];
+    }
+    
+    return ans[n];
+}
+
+int main()
+{
+	int n;
+	cin >> n;
+	cout << staircase(n);
+}
